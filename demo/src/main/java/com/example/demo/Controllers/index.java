88ba2f2d3db1cf82d255ed62284bb61b7a7a8f89
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-08-21 18:44:32 -0500

    Create first template

--- demo/src/main/java/com/example/demo/Controllers/index.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-
-public class index {
-        
-}
--- practica1/src/test/java/com/example/practica1/Controllers/index.java
+++
@@ -1,7 +0,0 @@
-package com.example.practica1.Controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class index {
-}

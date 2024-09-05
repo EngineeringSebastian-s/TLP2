d75a274601f03aa7be89faa3871844f230b250b1
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-09-04 19:55:56 -0500

    optimize imports

--- demo/src/main/java/com/example/demo/Controllers/indexController.java
+++ demo/src/main/java/com/example/demo/Controllers/indexController.java
@@ -10,8 +10,6 @@ import com.example.demo.Models.entity.Usuario;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller

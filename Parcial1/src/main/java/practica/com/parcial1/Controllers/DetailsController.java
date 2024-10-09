abeec11c984cba6c12af4c30f7f97fdd3e5cd03a
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-10-09 16:59:01 -0500

    implement collpase in purchase

--- Parcial1/src/main/java/practica/com/parcial1/Controllers/DetailsController.java
+++ Parcial1/src/main/java/practica/com/parcial1/Controllers/DetailsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import practica.com.parcial1.Models.DAO.details.IDetailsDao;
@@ -14,10 +15,10 @@ public class DetailsController {
     @Autowired
     private IDetailsDao detailsDao;
 
-    @GetMapping({"/",""})
-    public String List(Model model, @RequestParam(required = false) boolean confirmDel, @RequestParam(required = false) boolean confirmEdt){
+    @GetMapping({"/{id}"})
+    public String List(@PathVariable Long id, Model model, @RequestParam(required = false) boolean confirmDel, @RequestParam(required = false) boolean confirmEdt){
         model.addAttribute("Title", "Detalles de Ventas");
-        model.addAttribute("details", detailsDao.findAll());
+        model.addAttribute("details", detailsDao.findOne(id));
         model.addAttribute("confirmDel", confirmDel);
         model.addAttribute("confirmEdt", confirmEdt);
         return "/Detalles/List";

0f73a0067fae4b3948a7843ae96d55eb55a93255
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-10-17 19:45:29 -0500

    try with https requests

---
+++ SupaBase/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (name, lastname, email, password, role) VALUES ('Juan', 'Pérez', 'juan.perez@example.com', 'Password123!', 'USER');
+INSERT INTO usuarios (name, lastname, email, password, role) VALUES('María', 'Gómez', 'maria.gomez@example.com', 'SecurePass!9', 'ADMIN');
+INSERT INTO usuarios (name, lastname, email, password, role) VALUES('Luis', 'Martínez', 'luis.martinez@example.com', 'MyPassword@2023', 'USER');
+INSERT INTO usuarios (name, lastname, email, password, role) VALUES('Ana', 'Lopez', 'ana.lopez@example.com', 'Passw0rd!', 'USER');
+INSERT INTO usuarios (name, lastname, email, password, role) VALUES('Carlos', 'Sanchez', 'carlos.sanchez@example.com', 'C@rloS123', 'ADMIN');
